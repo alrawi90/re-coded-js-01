f58152c6d0235f2c3d7e697d29c497d256ef454e
Ali Alrawi <ali.developer90@gmail.com>
2017-02-28 09:59:19 +0300

    done

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-  
+
+
+	var current=()=> $('#textArea').val();
+	var clear=()=>{$('#myDiv').html(null)};
+	var backup=()=>$('#myDiv').html();
+	  $('textArea').on('keydown',function(e){
+        current().clear().backup();
+        backup().current().clear();
+        ////////////////////
+        /////////////////
+        //////////////
+        ///////////
+        ///////
+        /////
+        ///
+        //
+  }
+
 });
+
+})
+
+
